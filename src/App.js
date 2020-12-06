aa24b2d057e4ba3670d051c1552189314173e1db
sourabh <sourabhraghav30@gmail.com>
2020-12-06 17:42:28 +0530

    Added InfoBox Component into Header Component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Map from "./Map";
-import InfoBox from "./InfoBox";
 import Sidebar from "./Sidebar";
 import { Card, CardContent } from "@material-ui/core";
 function App() {
@@ -12,11 +11,6 @@ function App() {
     <div className="app">
       <div className="app_left">
         <Header />
-        <div className="app_stats">
-          <InfoBox title="Coronavirus cases" cases="1200" total="1M" />
-          <InfoBox title="Recovered" cases="1655" total="2M" />
-          <InfoBox title="Deaths" cases="1091" total="16356" />
-        </div>
         <Map />
       </div>
       <Card className="app_right">
@@ -29,3 +23,23 @@ function App() {
 }
 
 export default App;
+
+{
+  /* <div className="app_stats">
+          <InfoBox
+            title="Coronavirus cases"
+            cases={countryInfo.todayCases}
+            total={countryInfo.cases}
+          />
+          <InfoBox
+            title="Recovered"
+            cases={countryInfo.todayRecovered}
+            total={countryInfo.recovered}
+          />
+          <InfoBox
+            title="Deaths"
+            cases={countryInfo.todayDeaths}
+            total={countryInfo.deaths}
+          />
+        </div> */
+}
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,12 @@
 import { FormControl, MenuItem, Select } from "@material-ui/core";
 import React, { useState, useEffect } from "react";
 import "./Header.css";
+import InfoBox from "./InfoBox";
 function Header() {
   //STATE => how to a write a variable in react <<<<
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("Worldwide");
+  const [countryInfo, setCountryInfo] = useState({});
   //USEEFFECT => Run a piece of code based on a given condition.
   useEffect(() => {
     // async -> send a request, wait for it, fetch the URL(data), do something with the data.
@@ -25,26 +27,55 @@ function Header() {
   const onCountryChange = async (event) => {
     const countryCode = event.target.value;
     // console.log("this is country:", countryCode);
-    setCountry(countryCode);
+    const url =
+      countryCode === "Worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountry(countryCode);
+        setCountryInfo(data);
+      });
   };
-
+  console.log("CountryInfo:", countryInfo);
   return (
-    <div className="header">
-      <h2 className="header_name">COVID-19 TRACKER</h2>
-      <FormControl className="header_dropdown">
-        <Select variant="outlined" onChange={onCountryChange} value={country}>
-          {/* Loop through all the countries and
+    <div>
+      <div className="header">
+        <h2 className="header_name">COVID-19 TRACKER</h2>
+        <FormControl className="header_dropdown">
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
+            {/* Loop through all the countries and
              show the dropdown list of all options */}
-          <MenuItem value="Worldwide">Worldwide</MenuItem>
-          {countries.map((country) => (
-            <MenuItem value={country.value}>{country.name}</MenuItem>
-          ))}
+            <MenuItem value="Worldwide">Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
 
-          {/* <MenuItem value="Worldwide">option2</MenuItem>
+            {/* <MenuItem value="Worldwide">option2</MenuItem>
           <MenuItem value="Worldwide">option3</MenuItem>
           <MenuItem value="Worldwide">option4</MenuItem> */}
-        </Select>
-      </FormControl>
+          </Select>
+        </FormControl>
+      </div>
+      <div className="app_stats">
+        <InfoBox
+          title="Coronavirus cases"
+          cases={countryInfo.todayCases}
+          total={countryInfo.cases}
+        />
+        <InfoBox
+          title="Recovered"
+          cases={countryInfo.todayRecovered}
+          total={countryInfo.recovered}
+        />
+        <InfoBox
+          title="Deaths"
+          cases={countryInfo.todayDeaths}
+          total={countryInfo.deaths}
+        />
+      </div>
     </div>
   );
 }
